6e904ab4fcd07e529809b359623132c892410864
muzamilmeerdev <127821271+muzamilmeerdev@users.noreply.github.com>
2026-05-10 14:36:36 +0530

    Add image with link to CultureHistory component

--- src/app/components/CultureHistory.tsx
+++ src/app/components/CultureHistory.tsx
@@ -1,1 +1,8 @@
 
+<a href="https://res.cloudinary.com/dxjkbpmgm/image/upload/v1778400212/IMG_20260506_210239_f2wwv8.png" target="_blank">
+  <img
+    src="https://res.cloudinary.com/dxjkbpmgm/image/upload/v1778400212/IMG_20260506_210239_f2wwv8.png"
+    alt="Image"
+    style="cursor:pointer;"
+  >
+</a>
