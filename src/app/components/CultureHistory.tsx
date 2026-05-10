461f7bf6e426ea91c51c0ed735a76e91fa243ae3
muzamilmeerdev <127821271+muzamilmeerdev@users.noreply.github.com>
2026-05-10 14:37:53 +0530

    Fix closing anchor tag in CultureHistory component

--- src/app/components/CultureHistory.tsx
+++ src/app/components/CultureHistory.tsx
@@ -1,4 +1,3 @@
-
 <a href="https://res.cloudinary.com/dxjkbpmgm/image/upload/v1778400212/IMG_20260506_210239_f2wwv8.png" target="_blank">
   <img
     src="https://res.cloudinary.com/dxjkbpmgm/image/upload/v1778400212/IMG_20260506_210239_f2wwv8.png"
